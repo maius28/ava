c5ce37dc762de98551a063ef5ed20858e905bd9f
maius28 <185414515@qq.com>
2025-07-08 18:07:39 +0800

    ee

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    // 确保 HTML 文件不被合并或重命名
+    rollupOptions: {
+      input: {
+        main: './index.html', // 明确指定入口 HTML
+      },
+    },
+  },
   // 设置开发服务器代理，临时解决跨域问题
   server: {
     proxy: {
